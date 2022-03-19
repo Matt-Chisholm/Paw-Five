928d504b3790b8d55bc5d5fc1be491db6bee4ef1
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-18 21:51:20 -0700

    resolve conflict

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
 import Example from './components/example';
-<<<<<<< HEAD
 import Recorder from './components/Recorder';
-=======
 import NavBar from './components/NavBar';
->>>>>>> master
 
 export default function App(props) {
 
@@ -14,11 +11,8 @@ export default function App(props) {
     <div className="App">
         <h1>Paw Five</h1>
         <Example />
-<<<<<<< HEAD
         <Recorder />
-=======
         <NavBar />
->>>>>>> master
 
       </div>
   )
