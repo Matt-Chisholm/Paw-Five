2019288a591ab49d426bbd49fb31177d39382aa9
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-19 16:51:00 -0700

    add Profile and Skills components; add routes for dog and dog's skills

--- express-back-end/routes/profile.js
+++ express-back-end/routes/profile.js
@@ -3,12 +3,35 @@ const router  = express.Router();
 
 module.exports = (db) => {
 // Route: /api/profile/
-  router.get("/", (req, res) => {
-    console.log("touchdown");
-    res.render("/", {});
+  router.get("/dog/:id", (req, res) => {
+    const user_id = req.params.id;
+    db.query(`
+      SELECT dogs.id, name, breed, avatar, username
+      FROM dogs
+      JOIN users ON dogs.user_id = users.id
+      WHERE users.id = $1;
+    `, [user_id])
+      .then(result => {
+        res.send(result.rows[0]);
+      })
+      .catch(err => console.log("error", err));
+  });
+
+  router.get("/skills/:id", (req, res) => {
+    const dog_id = req.params.id;
+    db.query(`
+      SELECT skills.name, description
+      FROM skills
+      JOIN dogs ON dogs.id = skills.dog_id
+      WHERE dogs.id = $1;
+    `, [dog_id])
+      .then(result => {
+        res.send(result.rows);
+      })
+      .catch(err => console.log("error", err));
   });
 
 
-  
+
   return router;
 };
