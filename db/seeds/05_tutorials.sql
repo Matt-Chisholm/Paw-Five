3b077fc9f05ce000a65c14c5037f3d7e4262fbf0
Matheson94 <mathesonchisholm@hotmail.com>
2022-03-22 03:35:51 +0000

    added bailey videos

--- db/seeds/05_tutorials.sql
+++ db/seeds/05_tutorials.sql
@@ -1,2 +1,6 @@
 INSERT INTO tutorials (id, description, timestamp, video_path) VALUES (1, 'Trying to sit, being stubborn.', NOW()::timestamp, 'https://www.kapwing.com/e/62393b57313570006d1eff18');
 
+INSERT INTO tutorials (id, description, timestamp, video_path) VALUES (2, 'Bailey is a beautiful singer.', NOW()::timestamp, 'https://www.kapwing.com/e/623942171c877700c4a93070');
+INSERT INTO tutorials (id, description, timestamp, video_path) VALUES (3, 'Have you seen Squid Game?', NOW()::timestamp, 'https://www.kapwing.com/e/6239432f2d096200f105ca0b');
+
+https://www.kapwing.com/e/6239432f2d096200f105ca0b
