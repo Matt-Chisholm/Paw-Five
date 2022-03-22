117609feb3a788764a91a69538a460ae607d59ab
TJ Phan <t.jamesphan@gmail.com>
2022-03-22 13:57:36 -0700

    added profile ring

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Home.scss';
+import ring from './images/home-profile-ring.svg'
 
 export default function Home () {
 
@@ -9,7 +10,7 @@ export default function Home () {
   return (
     <div>
       <div className='home-top'>
-        
+        <img src={ring} alt="home-profile-ring"/>
       </div>
       <div className='home-bottom'>
 
