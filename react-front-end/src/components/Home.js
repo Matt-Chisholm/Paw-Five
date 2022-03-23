75d7b47571afd0752c32acba22dbae1337cc1d5f
TJ Phan <t.jamesphan@gmail.com>
2022-03-22 22:00:22 -0700

    adding useEffect for cookie on Home Component

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -3,6 +3,20 @@ import './Home.scss';
 import ring from './images/home-profile-ring.svg'
 
 export default function Home () {
+  const [ user, setUser ] = useState("") 
+  
+
+  // // eventually get all the data. for now get all users.
+  // useEffect(() =>{
+  //   Axios
+  //     .get(`api/routes/home/users/${props.user_id}`)
+  //       .then(success => {
+  //         console.log("HOME useEff", success);
+  //         // setUser()
+  //       })
+  //       .catch(); 
+  // }, []);
+
 
 
 
