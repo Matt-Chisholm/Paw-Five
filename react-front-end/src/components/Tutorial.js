3d0f3532620753dcfea7c975cd7022cd176143b9
TJ Phan <t.jamesphan@gmail.com>
2022-03-23 11:45:35 -0700

    adding class to distinguish selected

--- react-front-end/src/components/Tutorial.js
+++ react-front-end/src/components/Tutorial.js
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import './Tutorial.scss';
+import classname from 'classnames'
 
 export default function Tutorial() {
   const [tutorials, setTutorials] = useState([]);
+  const [ selected, setSelected ] = useState( selected || "nothing") 
   useEffect(() => {
     axios.get(`/api/tutorials`).then((response) => {
       console.log(response.data);
@@ -12,12 +14,20 @@ export default function Tutorial() {
     });
   }, []);
 
-
+  // what defines selected? when the key equals to the selected
+  //  on click => setSelected to key. if key matches to selected, then change class
+  
+  // crate classname if selected change dimensions
+  const selectedVideo = classname('video-responsive', {'video-responsive-selected': selected === 0})
   const renderTutorials = () => {
     return tutorials.map((tut, index) => {
+      const selectVideo = (index) => {
+        console.log("TJ");
+        setSelected(index)
+      }
       return (
 				<>
-					<div key={index} className='video-responsive'>
+					<div key={index} className={selectedVideo} onClick={()=>selectVideo(index)}>
 						<iframe
 							src={tut.video_path}
 							frameBorder='0'
@@ -35,6 +45,7 @@ export default function Tutorial() {
       
   return (
     <div className='tut-container'>
+        {selected}
         {renderTutorials()}
     </div>
   )
