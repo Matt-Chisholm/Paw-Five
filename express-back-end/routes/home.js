47a1a68b1f4c54edb8e040c220981dbc43e00977
TJ Phan <t.jamesphan@gmail.com>
2022-03-22 20:55:58 -0700

    created route for user image

--- express-back-end/routes/home.js
+++ express-back-end/routes/home.js
@@ -4,12 +4,36 @@ const router = express.Router();
 
 module.exports = (db) => {
 
+  // test route
+  router.get("/", (req, res) => {
+    db.query(`
+      SELECT * 
+      FROM users;
+    `)
+    .then(result => {
+      res.send(result)
+    })
+    .catch(error =>{
+      res.send(error)
+    })
+  });
+
+  // 
   router.get("/users/image/:id", (req, res) =>{
     const userId = req.params.id;
     db.query(`
-    `, [])
-    .then()
-    .catch()
+      SELECT image 
+      FROM users
+      WHERE users.id = $1;
+    `, [userId])
+    .then(result => {
+      res.send(result.rows)
+      console.log("query", result);
+    })
+    .catch(error => {
+      console.log("query error", error);
+      res.send(error)
+    })
   });
 
   return router;
