ee555b4f6d92a087a02126f4dd200265590b9de8
TJ Phan <t.jamesphan@gmail.com>
2022-03-23 17:02:23 -0700

    added Rainbow component for chart

---
+++ react-front-end/src/components/Rainbow.js
@@ -0,0 +1,12 @@
+import react from 'react'
+import { RadialBarChart, RadialBar, Legend } from "recharts";
+
+export default function Rainbow (){
+
+  return (
+    <div>
+      
+    </div>
+  )
+
+}
