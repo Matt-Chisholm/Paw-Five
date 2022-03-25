18ffdcbffd2fb6b0e328ea12b51623d957077445
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-25 13:26:10 -0700

    change button text

--- react-front-end/src/components/TutorialDetails.js
+++ react-front-end/src/components/TutorialDetails.js
@@ -1,12 +1,40 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
+import axios from "axios";
 import "./TutorialDetails.scss";
+import sit from "./images/sit.svg";
 
 
 export default function TutorialDetails(props) {
+  const [tutorial, setTutorial] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/tutorial/details/${props.tutorial_id}`).then((response) => {
+      console.log("success from TutorialDetails.js component useEffect:", response.data);
+      setTutorial(response.data);
+    })
+      .catch(error => {
+        console.log("error from Tutorial.js component useEffect: ", error);
+      });
+  }, []);
 
   return (
-    <div className="tutorial-details" onClick={props.onChange}>
-      {props.tutorial_id}
+    <div className="tutorial-details">
+      <div
+        className={`tutorial-item ${tutorial.level}`}
+      >
+        <div className="icon">
+          {/* {tutorial.icon} */}
+          <img className="img-icon" src={sit} />
+        </div>
+        <div className="right-side">
+          <div className="title">
+            {tutorial.name}
+          </div>
+          <div className="level">
+            <p>Level: {tutorial.level}</p>
+          </div>
+        </div >
+      </div>
     </div>
   )
 }
--- react-front-end/src/components/TutorialItem.js
+++ react-front-end/src/components/TutorialItem.js
@@ -4,13 +4,18 @@ import sit from "./images/sit.svg";
 
 export default function TutorialItem(props) {
   return (
-    <div 
-    className={`tutorial-item ${props.level}`}
-    onClick={() => props.onChange(props.tutorial_id)}
+    <div
+      className={`tutorial-item ${props.level}`}
+      onClick={() => {
+        console.log("tut", props.tutorial_id);
+        props.onClick();
+        props.changeBackButtonAction();
+        props.changeBackButtonText("Back to Tutorials");
+      }}
     >
       <div className="icon">
         {/* {props.icon} */}
-        <img className="img-icon" src={sit}/>
+        <img className="img-icon" src={sit} />
       </div>
       <div className="right-side">
         <div className="title">
