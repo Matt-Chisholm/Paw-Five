27e8027856b45b6672ccc2065f7ba2f3b3fb9d95
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-03-27 16:32:35 -0700

    styling for created session

--- react-front-end/src/components/CreatedSession.js
+++ react-front-end/src/components/CreatedSession.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import lvlup from "./images/levelup.png";
+import './CreatedSession.scss';
 
 export default function CreatedSession(props) {
   const [displayedSesh, setDisplayedSesh] = useState();
@@ -11,16 +13,21 @@ export default function CreatedSession(props) {
     });
   }, []);
   return (
-    <div>
-      {typeof displayedSesh === 'object' && 
-      <div className="session-container">
-      <h2 className="sesh-title">Created Session!</h2>
-        <img className="session-image" src={displayedSesh.avatar} alt="" />
-        <h3>Dog Name: {displayedSesh.dog_name}</h3>
-        <h3>{displayedSesh.name}</h3>
-        <h3>Skill trained: {displayedSesh.skill_name}</h3>
-        <h3>How it went: {displayedSesh.result}</h3>
-      </div>}
+    <div className="created">
+      {typeof displayedSesh === "object" && (
+        <div className="session-container">
+          <h2 className="sesh-title">Created Session!</h2>
+          <img className="session-image" src={displayedSesh.avatar} alt="" />
+          <h3 className="sesh-text">Well Done!</h3>
+          <h3 className="sesh-text">
+            {displayedSesh.dog_name}'s {displayedSesh.skill_name} skill has
+            leveled up!
+          </h3>
+          <div className="arrow bounce">
+            <img className="lvlup" src={lvlup} alt="" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
