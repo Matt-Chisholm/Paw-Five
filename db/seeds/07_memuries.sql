5d45cc8675b76f81baa9d93fe99c7e423e3061a1
TJ Phan <t.jamesphan@gmail.com>
2022-03-29 10:00:59 -0700

    resetted table for memuries and build seed file for easy reset

---
+++ db/seeds/07_memuries.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Birdie', 2, 'https://media.discordapp.net/attachments/952978925259206699/957322491234254868/IMG_1063.jpg?width=495&height=660');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Birdie', 2, 'https://media.discordapp.net/attachments/952978925259206699/957322491569786970/IMG_1062.jpg?width=528&height=660');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Bailey', 2, 'https://media.discordapp.net/attachments/952978925259206699/957114663601078322/IMG_4239.jpg?width=660&height=660');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, 'https://cdn.discordapp.com/attachments/952978925259206699/958116179401134090/got100.jpg');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Birdie', 2, 'https://scontent.fcxh3-1.fna.fbcdn.net/v/t39.30808-6/272381715_2141160102707910_3362348365552711380_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=gC6hXpJ5IEYAX-ol8lq&_nc_ht=scontent.fcxh3-1.fna&oh=00_AT_GtehpK9PXGvA4prXEKdMmsCUE6oSJM8WIxxtdhRjRWg&oe=6243EA92');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Bolt', 3, 'http://2.bp.blogspot.com/-SRON9gcWtOI/VCcN7RCkAUI/AAAAAAAAGnA/QQbTzMy-vjk/s1600/Bolt.jpg');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Bolt', 3, 'https://www.awn.com/sites/default/files/styles/inline/public/image/featured/3856-bolt-disney-gets-reborn-again.jpg?itok=gaHB8-ys');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
+INSERT INTO memuries (name, dog_id, photo) VALUES ('Simon', 2, '');
--- db/seeds/08_memuries.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO memuries (name, dog_id, photo) VALUES ('Birdie', 2, 'https://media.discordapp.net/attachments/952978925259206699/957322491234254868/IMG_1063.jpg?width=495&height=660');
-INSERT INTO memuries (name, dog_id, photo) VALUES ('Birdie', 2, 'https://media.discordapp.net/attachments/952978925259206699/957322491569786970/IMG_1062.jpg?width=528&height=660');
-INSERT INTO memuries (name, dog_id, photo) VALUES ('Bailey', 2, 'https://media.discordapp.net/attachments/952978925259206699/957114663601078322/IMG_4239.jpg?width=660&height=660');
