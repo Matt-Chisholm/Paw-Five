e99f2e0397f33d808dbeaf4407d3ae56fd51be31
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-31 10:31:30 -0700

    >=100 skill level medal

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (username, email, password) VALUES ('TJ', 'thunder_justice_psi@gmail.com', 'password');
+INSERT INTO users (username, email, password, image) VALUES ('TJ', 'thunder_justice_psi@gmail.com', 'password', 'https://media.discordapp.net/attachments/952978925259206699/956228105884799046/avatar-test_1.jpeg');
 INSERT INTO users (username, email, password) VALUES ('Matt', 'matias_chromewalker@gmail.com', 'password');
 INSERT INTO users (username, email, password) VALUES ('Gleb', 'glebulon_skystorm@gmail.com', 'password');
