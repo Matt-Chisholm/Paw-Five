1871fcbc74fe1417d938b9fdf67c4e58688f8df1
Gleb Shkut <gleb.shkut@gmail.com>
2022-04-07 11:19:46 -0700

    add errors for LogIn page fields

--- react-front-end/src/components/UserProcessing/LogIn.js
+++ react-front-end/src/components/UserProcessing/LogIn.js
@@ -7,25 +7,52 @@ export default function LogIn(props) {
 
   // States for checking the errors
   const [error, setError] = useState("ok");
+  const [labelError, setLabelError] = useState([]);
 
   // Handling the email change
   const handleEmail = (e) => {
     setEmail(e.target.value);
     setError("ok");
+    if (email.length >= 8 && email.includes("@")) {
+      const index = labelError.indexOf("email-error");
+      // deleting email-error from errors array
+      if (index !== -1) {
+        const copyErrors = [...labelError];
+        copyErrors.splice(index, 1);
+        setLabelError(copyErrors);
+      }
+    }
   };
-
+  
   // Handling the password change
   const handlePassword = (e) => {
     setPassword(e.target.value);
     setError("ok");
+    if (password.length > 7) {
+      const index = labelError.indexOf("password-error");
+      // deleting pasword-error from errors array
+      if (index !== -1) {
+        const copyErrors = [...labelError];
+        copyErrors.splice(index, 1);
+        setLabelError(copyErrors);
+      }
+    }
   };
 
   // Handling the form submission
   const handleSubmit = (e) => {
+    setLabelError([]);
+    let noErrors = true;
     e.preventDefault();
-    if (email.length < 8 || !email.includes("@") || password.length < 8) {
-      setError("input-error");
-    } else {
+    if (email.length < 8 || !email.includes("@")) {
+      setLabelError(prev => [...prev, "email-error"]);
+      noErrors = false;
+    }
+    if (password.length < 8) {
+      setLabelError(prev => [...prev, "password-error"]);
+      noErrors = false;
+    }
+    if (noErrors) {
       axios.post("/api/userProcessing/login/", {
         email: email,
         password: password
@@ -68,9 +95,7 @@ export default function LogIn(props) {
 
         {error !== "ok" &&
           <div className="messages" onClick={() => setError("ok")}>
-            {(error === "input-error") ?
-              errorMessage("Please enter all the fields") : errorMessage("User doesn't exist or data is incorrect")
-            }
+            {(error === "wrong-data") && errorMessage("User doesn't exist or data is incorrect")}
           </div>
         }
       </div>
@@ -81,12 +106,14 @@ export default function LogIn(props) {
           <label className="label">Email</label>
           <input onChange={handleEmail} className="input"
             value={email} type="email" />
+          {(labelError.includes("email-error")) && <label className="label-error">Email should contain '@' sign and at least 8 characters</label>}
         </div >
 
         <div className="text-field">
           <label className="label">Password</label>
           <input onChange={handlePassword} className="input"
             value={password} type="password" />
+          {(labelError.includes("password-error")) && <label className="label-error">Password should contain at least 8 characters</label>}
         </div >
 
         <div id="submit">
