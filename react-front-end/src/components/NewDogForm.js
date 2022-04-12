37d98165589f8d53975ea98f24ca796202949b35
Gleb Shkut <gleb.shkut@gmail.com>
2022-04-12 13:11:26 -0700

    state for input field create

--- react-front-end/src/components/NewDogForm.js
+++ react-front-end/src/components/NewDogForm.js
@@ -1,13 +1,20 @@
-import react from "react";
+import react, { useState } from "react";
 
 export default function NewDogForm(props) {
+  const [name, setName] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
 
   return (
-    <div className='add-dog-form' onClick={() => setAddDogDisplay(false)}>
-      <div className='text-field'>
-        <label className=''></label>
-      </div>
+    <form className='add-dog-form'>
       <span className='add-a-dog'>Add a dog form</span>
-    </div>
+      <div className='text-field'>
+        <label className='dog-name'>Dog Name</label>
+        <input className='dog-name-input' onChange={handleName} />
+      </div>
+      <span onClick={() => props.setAddDogDisplay(false)}>Cancel</span>
+    </form>
   )
 }
--- react-front-end/src/components/Profile.js
+++ react-front-end/src/components/Profile.js
@@ -132,7 +132,7 @@ export default function Profile(props) {
                     </div>
                   }
                   {addDogDisplay === true &&
-                    <NewDogForm />
+                    <NewDogForm setAddDogDisplay={setAddDogDisplay} />
                   }
                 </div>
               }
