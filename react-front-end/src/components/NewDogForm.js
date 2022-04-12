349686940613e63c0e52f5cf80dcaa7cecfbe8ce
Gleb Shkut <gleb.shkut@gmail.com>
2022-04-12 13:47:09 -0700

    handling photo

--- react-front-end/src/components/NewDogForm.js
+++ react-front-end/src/components/NewDogForm.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import react, { useState } from "react";
+import "./NewDogForm.scss";
 
 export default function NewDogForm(props) {
   const [name, setName] = useState("");
@@ -31,14 +32,14 @@ export default function NewDogForm(props) {
       setLabelError(prev => [...prev, "breed-error"]);
       noErrors = false;
     }
-    if (photo.length === 0) {
-      setPhoto("https://static.vecteezy.com/system/resources/previews/005/055/092/original/cute-australian-shepherd-dog-avatar-cartoon-icon-illustration-vector.jpg");
-    }
+    // if (photo.length === 0 && noErrors) {
+    //   setPhoto("https://static.vecteezy.com/system/resources/previews/005/055/092/original/cute-australian-shepherd-dog-avatar-cartoon-icon-illustration-vector.jpg");
+    // }
     // if (noErrors) {
     //   axios.post("/api/userProcessing/register/", {
     //     username: name,
     //     email: email,
-    //     password: password
+    //     photo: photo.length === 0 ? "https://static.vecteezy.com/system/resources/previews/005/055/092/original/cute-australian-shepherd-dog-avatar-cartoon-icon-illustration-vector.jpg" : photo
     //   }).then((response) => {
     //     localStorage.setItem("username", name);
     //     props.setCookie("user_id", Number(response.data.id));
@@ -64,7 +65,7 @@ export default function NewDogForm(props) {
         <label className='dog-photo'>Photo</label>
         <input className='dog-photo-input' value={photo} type="text" onChange={handlePhoto} />
       </div>
-      <button id="submit" onClick={handleSubmit}></button>
+      <button id="submit" onClick={handleSubmit}>Submit</button>
       <span onClick={() => props.setAddDogDisplay(false)}>Cancel</span>
     </form>
   )
