24d7535d5184160773f93ec38c868628f564640a
Gleb Shkut <gleb.shkut@gmail.com>
2022-04-14 17:38:16 -0700

    add full-screen for image by click

--- react-front-end/src/components/HomeNavigationBar/Memuries.js
+++ react-front-end/src/components/HomeNavigationBar/Memuries.js
@@ -7,6 +7,7 @@ import NewMemury from "./NewMemury";
 export default function Memuries(props) {
   const [memuries, setMemuries] = useState([]);
   const [render, setRender] = useState(false);
+  const [fullScreenImage, setFullScreenImage] = useState(null);
 
   useEffect(() => {
     axios.get(`/api/home/memuries`).then((response) => {
@@ -18,7 +19,7 @@ export default function Memuries(props) {
   const renderMemuries = (memuries) => {
     return memuries.reverse().map((mem, index) => {
       return (
-        <div key={index} className="memury-unit">
+        <div key={index} className="memury-unit" onClick={() => setFullScreenImage(index)}>
           <img name={mem.name} src={mem.photo} alt={index} />
         </div>
       );
@@ -32,10 +33,11 @@ export default function Memuries(props) {
         <NewMemury render={render} setRender={() => setRender(!render)} />
         <h1 className="mem-text"> Keep going!</h1>
       </section>
+      {fullScreenImage !== null && <img className="full-screen-image" src={memuries[fullScreenImage].photo} onClick={() => setFullScreenImage(null)} />}
       <section className="memuries__bottom">
         {renderMemuries(memuries)}
       </section>
-      <br/>
+      <br />
       <h3 className="mem-text">You've dug to the bottom. Woof.</h3>
     </div>
   );
